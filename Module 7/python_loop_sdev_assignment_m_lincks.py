50791b1e50041b1d8a6cd81a0849775f7ada6a85
Madison Lincks <mlincks3601@gmail.com>
2025-04-30 13:09:48 -0400

    Create python_loop_sdev_assignment_m_lincks.py
    Python loop homework M.lincks

---
+++ Module 7/python_loop_sdev_assignment_m_lincks.py
@@ -0,0 +1,9 @@
+# Create a list of 15 numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+# Loop through the list
+for number in numbers:
+    if number % 2 == 0:
+        print(number, "is even")
+    else:
+        print(number, "is odd") 
